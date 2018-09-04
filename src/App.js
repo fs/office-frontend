e8db6f7c446fc58153c130ebce47751d578cf024
Gornyy Vladimir <vladimir.gornyy@flatstack.com>
2018-09-04 17:56:09 +0300

    Add material-ui

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
-import React, { Component } from 'react';
+import React, { Component, Fragment } from 'react';
+import CssBaseline from '@material-ui/core/CssBaseline';
 import AuthContainer from './containers/Auth/AuthContainer';
 
 class App extends Component {
   render() {
-    return <AuthContainer />;
+    return (
+      <Fragment>
+        <CssBaseline />
+        <AuthContainer />
+      </Fragment>
+    );
   }
 }
 
